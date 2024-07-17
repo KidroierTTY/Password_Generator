fee71534f4e8a0090270da4b89cf8817cacc3b19
KdroierTTY <kidroiertty@gmail.com>
2024-07-17 14:14:23 +0300

    Password_Generator.py Gotov

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,5 +1,9 @@
+import tkinter
+import random
+
+number_of_symbols = 8
+
 def generate(number_of_symbols=6):
-    import random
     symbols = []
     for i in range(number_of_symbols):
         num = random.randint(1, 3)
@@ -12,3 +16,29 @@ def generate(number_of_symbols=6):
     password = ''.join(str(symb) for symb in symbols)
     text.delete('1.0', tkinter.END)
     text.insert('1.0', password)
+
+window = tkinter.Tk()
+window.config(bg='black')
+window.geometry('800x600')
+window.title('Password')
+
+text = tkinter.Text(
+    font=('arial', 40, 'bold'),
+    height=1,
+    width=number_of_symbols + 1,
+    bg='black',
+    fg='white',
+    borderwidth=10
+)
+text.pack(expand=1)
+
+tkinter.Button(
+    text='Generate',
+    font=('arial', 40, 'bold'),
+    bg='black',
+    fg='white',
+    borderwidth=0,
+    command=lambda: generate(number_of_symbols)
+).pack(expand=1)
+
+window.mainloop()
